4d2f19778cb63c369fab204bfcb17e3f48d6e6e1
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-11-05 01:02:36 +0800

    mvp

--- lua/vertgrid/init.lua
+++ lua/vertgrid/init.lua
@@ -1,7 +1,46 @@
 local M = {}
 
+local size = 5
+local group = 'Vertgrid'
+local ns_id = vim.api.nvim_create_namespace('Vertns')
+vim.api.nvim_set_hl(ns_id, group, { bg = "#222222" })
+vim.api.nvim_create_augroup(group, { clear = true })
+
 function M.setup(opts)
-    print('hi vertgrid')
+    if not opts then opts = {} end
+    if opts.size then size = opts.size end
 end
+
+local function Highlight(target)
+    if target - 1 < 1 then return end
+    vim.api.nvim_win_set_hl_ns(0, ns_id)
+    vim.api.nvim_buf_set_extmark(0, ns_id, target - 1, 0, {
+        line_hl_group = group,
+    })
+end
+
+
+local function Render()
+    vim.api.nvim_buf_clear_highlight(0, ns_id, 1, -1)
+    local cursor_position = vim.api.nvim_win_get_cursor(0)[1]
+    local first_line = vim.fn.line('w0')
+    local last_line = vim.fn.line('w$')
+
+    for i = cursor_position, first_line, -size do
+        Highlight(i)
+    end
+    for i = cursor_position, last_line, size do
+        Highlight(i)
+    end
+end
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter", "CursorMoved" }, {
+    group = group,
+    callback = function()
+        vim.schedule(function()
+            Render()
+        end)
+    end
+})
 
 return M
